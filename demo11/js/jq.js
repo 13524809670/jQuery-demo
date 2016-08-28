02889dc2ffcb25a56cc4fa0e87966aad238ca652
wangrui <1634208912@qq.com>
2016-08-28 22:56:27 +0800

    first commit

---
+++ demo11/js/jq.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	$(".table .a").mouseover(function(){
+		$(".table .a").css("background-color","aqua");
+	});
+	$(".table .a").mouseout(function(){
+		$(".table .a").css("background-color","#dcf")
+	});
+
+	$(".table .b").mouseover(function(){
+		$(".table .b").css("background-color","#cf0");
+	});
+	$(".table .b").mouseout(function(){
+		$(".table .b").css("background-color","#cfd")
+	});
+
+	$(".table .c").mouseover(function(){
+		$(".table .c").css("background-color","aqua");
+	});
+	$(".table .c").mouseout(function(){
+		$(".table .c").css("background-color","#dcf")
+	});
+
+	$(".table .d").mouseover(function(){
+		$(".table .d").css("background-color","#cf0");
+	});
+	$(".table .d").mouseout(function(){
+		$(".table .d").css("background-color","#cfd")
+	});
+
+	$(".table .e").mouseover(function(){
+		$(".table .e").css("background-color","aqua");
+	});
+	$(".table .e").mouseout(function(){
+		$(".table .e").css("background-color","#dcf")
+	});
+});
+
+
